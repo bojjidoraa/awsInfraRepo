160c2947b2a55dab3165bf67d3a615d495b9d6d1
bojjidoraa <bojjidoraa@gmail.com>
2024-07-25 18:25:22 +0530

    Update sonar.tf

--- sonar.tf
+++ sonar.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "mySonarInstance" {
-      ami           = "ami-0ee23bfc74a881de5"
+      ami           = "ami-0b8fd93c15b2c81ce"
       key_name = var.key_name
-      instance_type = "t2.micro"
+      instance_type = "3.micro"
       vpc_security_group_ids = [aws_security_group.sonar-sg-2022.id]
       tags= {
         Name = "sonar_instance"
