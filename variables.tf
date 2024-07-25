b8f008f9dae92ab1395810c979c01098a60d9397
bojjidoraa <bojjidoraa@gmail.com>
2024-07-25 20:34:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "aws-sgroup-2024" 
+    default     = "jenkins-sgroup-dec-2021" 
 }
 
 variable "tag_name" { 
