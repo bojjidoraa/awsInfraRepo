b1bfc17dbbffb061cd384fc62316b7eee47c7b3c
bojjidoraa <bojjidoraa@gmail.com>
2024-07-25 10:52:19 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "azure-s3-bucket-for-tf-state"
-    key = "master"
+    key = "main"
     region = "eu-north-1"
     dynamodb_table = "azure-dynamo-db-table-tf"
   }
